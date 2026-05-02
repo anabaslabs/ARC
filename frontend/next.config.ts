1b6f49696f9fb51f248c03b9111d6742bc485eaf
Krishnendu Das <git@itskdhere.com>
2026-05-02 19:30:45 +0530

    some fixes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["*.devtunnels.ms", "*.ngrok-free.app"],
 };
 
 export default nextConfig;
